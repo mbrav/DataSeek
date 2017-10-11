df6563a593a3c68ce413545944a48a9fe63e07d6
mbrav <mbrav@protonmail.com>
2017-10-11 18:33:05 -0400

    mining working

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
-
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
@@ -18,7 +17,7 @@ serv.listen(2000);
 console.log('SERVER STARTED');
 
 var chatRecord = [];
-var clientRecord = {};
+var socketList = {};
 var clientsOnline = 0;
 
 // RECEIVING DATA
@@ -27,7 +26,9 @@ io.sockets.on('connection', function(socket) {
 	// setup the client once his settings are received
 	console.log("New Client");
 	clientsOnline++;
-	clientRecord[socket.id] = socket;
+
+	socket.dcMined = 0.0;
+	socketList[socket.id] = socket;
 	// update socket data when new position recieved from a player
 	socket.on('clientInit', function(msg) {
 		console.log("new client connected");
@@ -65,7 +66,7 @@ io.sockets.on('connection', function(socket) {
 		clientsOnline--;
 
 		// delete player
-		delete clientRecord[socket.id];
+		delete socketList[socket.id];
 
 		io.emit('clientDisconnect', {
 			clientsOnline: clientsOnline
@@ -74,11 +75,27 @@ io.sockets.on('connection', function(socket) {
 });
 
 // server message
-var refresh = 1; // set refresh rate (times per second)
+var refresh = 10; // set refresh rate (times per second)
 setInterval(function(){
+	var d = new Date();
+	var n = d.getMilliseconds();
 
+	for (var i in socketList) {
+		// some data manipulation
+		var old = socketList[i].dcMined;
+		socketList[i].dcMined = old * 1.0+(Math.cos(n/10)+1)/40;
+
+		// send private message to client
+		var socket = socketList[i];
+		socket.emit('serverPackage', {
+			dcMined : socketList[i].dcMined
+		});
+	}
+
+	// send public message to all clients
 	io.emit('serverMessage', {
-			msg: "Hello"
+			msg: "Hello Data Miners!",
+			clientsOnline: clientsOnline
 	});
 
 }, 1000/refresh);
--- client/script.js
+++ client/script.js
@@ -5,22 +5,66 @@ init();
 
 var clientsData;
 
-// P5.js
+var xspacing = 16; // Distance between each horizontal location
+var w; // Width of entire wave
+var theta = 0.0; // Start angle at 0
+var amplitude = 75.0; // Height of wave
+var period = 500.0; // How many pixels before the wave repeats
+var dx; // Value for incrementing x
+var yvalues; // Using an array to store height values for the wave
+
 function setup() {
+  // createCanvas(200, 200);
   createCanvas(window.innerWidth, window.innerHeight);
+  w = width + 16;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w / xspacing));
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-
+  background(0);
+  calcWave();
+  renderWave();
+  frameRate(10)
 }
-// Other
 
+function calcWave() {
+  // Increment theta (try different values for
+  // 'angular velocity' here)
+  theta += 0.06;
+
+  // For every x value, calculate a y value with sine function
+  var x = theta;
+  for (var i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x) * amplitude;
+    x += dx;
+  }
+}
+
+function renderWave() {
+  noStroke();
+  fill(255);
+  // A simple way to draw the wave with an ellipse at each location
+  for (var x = 0; x < yvalues.length; x++) {
+    ellipse(x * xspacing, height / 2 + yvalues[x], 16, 16);
+  }
+}
+
+// P5.js
+// function setup() {
+//   // createCanvas(window.innerWidth, window.innerHeight);
+// }
+//
+// function draw() {
+//   // if (mouseIsPressed) {
+//   //   fill(0);
+//   // } else {
+//   //   fill(255);
+//   // }
+//   // ellipse(mouseX, mouseY, 80, 80);
+// }
+
+// Other
 function init() {
   // send player settings
   socket.emit('clientInit', {
@@ -28,14 +72,14 @@ function init() {
   });
 
   // response from the server
-  socket.on('clientInitResponse', function(msg){
+  socket.on('clientInitResponse', function(msg) {
     clientsData = msg.clientRecord;
     for (var i = 0; i < msg.chatRecord.length; i++) {
       logConnectedUser(msg.chatRecord[i]);
     }
   });
 
-  socket.on('newClientBrodcast', function(msg){
+  socket.on('newClientBrodcast', function(msg) {
     clientsData = msg.clientRecord;
     clientsOnline = msg.clientsOnline;
     updateBasedOnClientsOnline(clientsOnline);
@@ -45,41 +89,52 @@ function init() {
     logConnectedUser(msg.chatRecord);
   });
 
-  socket.on('clientDisconnect', function(msg){
+  socket.on('clientDisconnect', function(msg) {
     updateBasedOnClientsOnline(clientsOnline);
 
     $('#terminal').append(
       $('<p>').html(
-        "<span>USER DISCONNECTS</span> <br> <b>"
-        + msg.clientsOnline
-        + "</b> users remain online"
+        "<span>USER DISCONNECTS</span> <br> <b>" +
+        msg.clientsOnline +
+        "</b> users remain online"
       ));
   });
 
-  socket.on('serverMessage', function(msg){
-    $('#terminal').append($('<p>').html(
-      "<i>"
-      + msg.msg
-      + "</i>"
-      ));
+  // Private Server Message
+  socket.on('serverPackage', function(msg) {
 
-      console.log(clientsData);
+    // sd mined
+    $('#dc-mined').html(msg.dcMined);
+
+  });
+
+
+  // Public Server Message
+  socket.on('serverMessage', function(msg) {
+    $('#terminal').append($('<p>').html(
+      "<i>" +
+      msg.msg +
+      "</i>"
+    ));
+
+    // sd client online
+    $('#client-count').html(truncateDecimals (msg.clientsOnline,2));
   });
 }
 
 function logConnectedUser(dataObject) {
   $('#terminal').append(
     $('<p>').html(
-      "<span>USER CONNECTS</span>"
-      + " <b> IP: </b>"
-      + dataObject.host
-      + " <b> TIME: </b>"
-      + dataObject.time
-      + " <b> AGENT: </b>"
-      + dataObject.agent
-      + "<br> <b> "
-      + dataObject.clientsOnline
-      + "</b> users remain online "
+      "<span>USER CONNECTS</span>" +
+      " <b> IP: </b>" +
+      dataObject.host +
+      " <b> TIME: </b>" +
+      dataObject.time +
+      " <b> AGENT: </b>" +
+      dataObject.agent +
+      "<br> <b> " +
+      dataObject.clientsOnline +
+      "</b> users remain online "
     )
   );
 }
@@ -90,5 +145,13 @@ function updateBasedOnClientsOnline(usersOnline) {
 }
 
 function opacityFormula(usersOnline) {
-  return Math.exp(usersOnline/60)-1;
+  return Math.exp(usersOnline / 60) - 1;
 }
+
+truncateDecimals = function (number, digits) {
+    var multiplier = Math.pow(10, digits),
+        adjustedNum = number * multiplier,
+        truncatedNum = Math[adjustedNum < 0 ? 'ceil' : 'floor'](adjustedNum);
+
+    return truncatedNum / multiplier;
+};
